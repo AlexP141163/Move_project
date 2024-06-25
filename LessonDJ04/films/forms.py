85f743cf10e7e5c7e047f8c780c6482e4a1dcd7a
AlexP141163 <pilyugin.aa@gmail.com>
2024-06-25 12:57:25 +0300

    add 2 an intermediate save

--- LessonDJ04/films/forms.py
+++ LessonDJ04/films/forms.py
@@ -1,7 +1,14 @@
 from .models import New_film
-from django.forms import ModelForm
+from django.forms import ModelForm, Textarea, TextInput, DateTimeInput
 
 class New_FilmForm(ModelForm):
     class Meta:
         model = New_film
         fields = ['title', 'description_film', 'feedback_film', 'pub_date', 'author']
+        widgets = {
+            'title': TextInput(attrs={'class':'form-control', 'placeholder': 'Заголовок фильма'}),
+            'description_film': Textarea(attrs={'class': 'form-control', 'placeholder': 'Описание фильма'}),
+            'feedback_film': Textarea(attrs={'class': 'form-control', 'placeholder': 'Отзыв о фильме'}),
+            'pub_date': DateTimeInput(attrs={'class': 'form-control', 'type': 'datetime-local', 'placeholder': 'Время публикации'}),
+            'author': TextInput(attrs={'class': 'form-control', 'placeholder': 'Автор'})
+        }
--- LessonDJ04/films/views.py
+++ LessonDJ04/films/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import New_film
 from .forms import New_FilmForm
 
@@ -11,5 +11,13 @@ def movies(request):
     return render(request, 'films/movies.html', {'films': films})
 
 def create_films(request):
+    error = ""
+    if request.method == 'POST':
+        form = New_FilmForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('films')
+        else:
+            error = "Данные были введены некорректно"
     form = New_FilmForm()
     return render(request, 'films/add_new_films.html', {'form':form})
